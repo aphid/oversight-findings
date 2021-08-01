65b36cbc5a2178ca65944d06520c88619b9b55c2
Abram Stern (aphid) <aphid@ucsc.edu>
2021-08-01 13:10:46 -0700

    unfinished changes from months ago

--- imgdump.js
+++ imgdump.js
@@ -5,16 +5,27 @@ var san = require("sanitize-filename");
 var fs = require('fs');
 var crypto = require('crypto');
 var moment = require("moment");
-var hearingsFile = "/var/www/oversightmachin.es/html/oversee/data/data.json";
-var hearings = JSON.parse(fs.readFileSync(hearingsFile));
 
 var settings = fs.readFileSync("settings.json");
+settings = JSON.parse(settings);
+console.log(settings);
 var outDir = settings.outDir;
 var hearingsFile = settings.hearingsFile;
 var encrypt;
 var serverOpts = {};
+console.log(hearingsFile)
+var hearings = JSON.parse(fs.readFileSync(hearingsFile));
 
-if (settings.privkey && settings.cert){
+var dirs = ["ocr", "spu", "unburn"];
+for (let d of dirs){
+    let target = outDir + "/" + d;
+    if (!fs.existsSync(target)){
+        fs.mkdirSync(target);
+    }
+}
+
+
+if (settings.privkey && settings.cert) {
     encrypt = true;
     method = https;
     serverOpts.key = fs.readFileSync(settings.privKey);
@@ -23,31 +34,31 @@ if (settings.privkey && settings.cert){
 
 
 
-var addPage = function(title,page){
-   for (let h of hearings.hearings){
-       for (let w of h.witnesses){
-           for (let p of w.pdfs){
-                if (p.title === title){
-                    if (!h.ocrPages){
-		        p.ocrPages = [];
-		    }
+var addPage = function (title, page) {
+    for (let h of hearings.hearings) {
+        for (let w of h.witnesses) {
+            for (let p of w.pdfs) {
+                if (p.title === title) {
+                    if (!h.ocrPages) {
+                        p.ocrPages = [];
+                    }
                     p.ocrPages.push(page);
-		}
-	   }
-       }
-   }
-   fs.writeFileSync(hearingsFile,JSON.stringify(hearings,undefined,2));
+                }
+            }
+        }
+    }
+    fs.writeFileSync(hearingsFile, JSON.stringify(hearings, undefined, 2));
 };
 
 
-var server = method.createServer(serverOpts, function(req, res) {
+var server = method.createServer(serverOpts, function (req, res) {
     console.log(moment().format());
     console.dir(req.param);
 
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Request-Method', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
-res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
     if (req.method == 'POST') {
         console.log("POST");
@@ -56,51 +67,62 @@ res.setHeader('Access-Control-Allow-Headers', '*');
             body += data.toString();
         });
         req.on('end', function () {
+            console.log(req.headers.referer);
+            if (req.headers.referer)
+            {
+
+            }
+            //need to determine which machine it is and pass it off to a handler
+
             var inData = JSON.parse(body);
-	    if (inData.timestamp1){
-               processSpu(inData);
+            if (inData.machine === "spu") {
+                processSpu(inData);
             } else {
-            console.log(inData.title, inData.root);
-            var ip = req.socket.remoteAddress;
-            var hash = crypto.createHash('md5').update(ip + new Date().toTimeString()).digest('hex');
-	 
-            if (inData.pageImg){
-            var out = outDir + san(inData.title + "_" + inData.page +  ".png");
-            console.log(out);
-            var img = inData.pageImg.replace(/^data:image\/png;base64,/, "");
-            fs.writeFileSync(out, img, 'base64');
-            addPage(inData.title,inData.page);
-            } else if (inData.words){
-            var out = outDir + san(inData.title + "_" + inData.page + ".json");
-            fs.writeFileSync(out,JSON.stringify(inData,undefined,2),"utf8");
-	    }
-        }});
-        res.writeHead(200, {'Content-Type': 'text/html'});
+                console.log(inData.title, inData.root);
+                var ip = req.socket.remoteAddress;
+                var hash = crypto.createHash('md5').update(ip + new Date().toTimeString()).digest('hex');
+
+                if (inData.pageImg) {
+                    var out = outDir + san(inData.title + "_" + inData.page + ".png");
+                    console.log(out);
+                    var img = inData.pageImg.replace(/^data:image\/png;base64,/, "");
+                    fs.writeFileSync(out, img, 'base64');
+                    addPage(inData.title, inData.page);
+                } else if (inData.words) {
+                    var out = outDir + san(inData.title + "_" + inData.page + ".json");
+                    fs.writeFileSync(out, JSON.stringify(inData, undefined, 2), "utf8");
+                }
+            }
+        });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end('post received');
     }
-    else
-    {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('what');
+    else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('what');
     }
 
 });
 
-function processSpu(data){
-  var out = outDir + san(data.page + "_" + data.timestamp1 + "_" + data.timestamp2 + ".png");
-  console.log("writing", out);
-  var img = data.pageImg.replace(/^data:image\/png;base64,/, "");
-  fs.writeFileSync(out, img, 'base64');
-  var send = [];
-  var datafile = (outDir + data.page + ".json");
-  if (fs.existsSync(datafile)){
-     send = JSON.parse(fs.readFileSync(datafile));
-  }
+function processSpu(data) {
+    for (let t of data.timestamps){
+        t = parseFloat(t);
+    }
+    console.log(data);
+    var out = outDir + san(data.page + "_" + data.timestamps[0] + "_" + data.timestamps[1] + ".png");
+    console.log("writing", out);
+    var img = data.pageImg.replace(/^data:image\/png;base64,/, "");
+    fs.writeFileSync(out, img, 'base64');
+    var send = [];
+    var datafile = (outDir + data.page + ".json");
+    if (fs.existsSync(datafile)) {
+        send = JSON.parse(fs.readFileSync(datafile));
+    }
 
-  send.push({ timestamp1: parseFloat(data.timestamp1), timestamp2: parseFloat(data.timestamp2), measure: parseFloat(data.measure), threshold: data.threshold});
-	  console.log(send);
-  fs.writeFileSync(datafile, JSON.stringify(send));
-  
+    send.push({ timestamps: data.timestamps, measure: parseFloat(data.measure), threshold: data.threshold });
+    console.log(send);
+    fs.writeFileSync(datafile, JSON.stringify(send));
+
 }
 port = 3000;
 server.listen(port);
