27641cbbe7aefa932d5ea3a8a86a6c13dd7f35b1
=Abram Stern (aphid) <aphid@ucsc.edu>
2024-09-08 11:24:22 -0700

    format

--- docs.js
+++ docs.js
@@ -2,51 +2,51 @@ let full, docs, hDocs;
 let fs = require("fs");
 let docspath = "/var/www/oversightmachin.es/html/ocr/";
 hDocs = [];
-let getData = async function(){
-  let f = fs.readFileSync("data.json");
-  full = JSON.parse(f);
-  let d = fs.readFileSync("docs.json");
-  docs = JSON.parse(d)
+let getData = async function () {
+   let f = fs.readFileSync("data.json");
+   full = JSON.parse(f);
+   let d = fs.readFileSync("docs.json");
+   docs = JSON.parse(d)
 }
 
-let Doc = function(o,w,h){
-  this.hearing = 
-  this.localName = o.localName;
-  this.localPath = o.localPath;
-  if (o.metadata.pdfinfo){
-     this.pages = o.metadata.pdfinfo.pages;
-  }
-  this.pageImages = o.pageImages;
-  this.shortName = o.shortName;
-  this.modes = ["tesseract_2.1.1", "ocrad_0.25"];
-  this.completedModes = [];
-  this.witness = w;
-  this.metadata = o.metadata;
-  delete this.witness.pdfs;
-  this.hearing = h;
-  delete this.hearing.witnesses;
-  delete this.hearing.video;
+let Doc = function (o, w, h) {
+   this.hearing =
+      this.localName = o.localName;
+   this.localPath = o.localPath;
+   if (o.metadata.pdfinfo) {
+      this.pages = o.metadata.pdfinfo.pages;
+   }
+   this.pageImages = o.pageImages;
+   this.shortName = o.shortName;
+   this.modes = ["tesseract_2.1.1", "ocrad_0.25"];
+   this.completedModes = [];
+   this.witness = w;
+   this.metadata = o.metadata;
+   delete this.witness.pdfs;
+   this.hearing = h;
+   delete this.hearing.witnesses;
+   delete this.hearing.video;
 }
 
 
-let makeHearDocs = async function(){
-  for (let hearing of full.hearings){
-     for (let witness of hearing.witnesses){
-	     for (let pdf of witness.pdfs){
-		if (pdf.needsScan){
-		    hDocs.push( new Doc(pdf, witness, hearing));
-		    console.log(pdf.localName);
-		 } else {
-                   //console.log("doesn't need scan");
-		 }
-	     }
-  }
-}
-  return Promise.resolve();
+let makeHearDocs = async function () {
+   for (let hearing of full.hearings) {
+      for (let witness of hearing.witnesses) {
+         for (let pdf of witness.pdfs) {
+            if (pdf.needsScan) {
+               hDocs.push(new Doc(pdf, witness, hearing));
+               console.log(pdf.localName);
+            } else {
+               //console.log("doesn't need scan");
+            }
+         }
+      }
+   }
+   return Promise.resolve();
 }
 
-let checkHearDocs = async function(){
-   for (let h of hDocs){
+let checkHearDocs = async function () {
+   for (let h of hDocs) {
       let cp = await h.checkPDF();
       let ci = await h.checkImages();
       let co = await h.checkOCR();
@@ -54,48 +54,48 @@ let checkHearDocs = async function(){
    return Promise.resolve();
 }
 
-Doc.prototype.checkPDF = async function(){
-     //console.log("checking for", this.localPath);
-     this.pdfChecked = fs.existsSync(this.localPath);
-     return Promise.resolve();
+Doc.prototype.checkPDF = async function () {
+   //console.log("checking for", this.localPath);
+   this.pdfChecked = fs.existsSync(this.localPath);
+   return Promise.resolve();
 }
 
-Doc.prototype.checkImages = async function(){
-   for (let p of this.pageImages){
+Doc.prototype.checkImages = async function () {
+   for (let p of this.pageImages) {
       //https://oversightmachin.es/overSSCIght/media/text/180515_0930_q-revanina-051518/180515_0930_q-revanina-051518_000.jpg
-      let ps = p.replace("https://oversightmachin.es","/mnt/oversee");
-      if (!fs.existsSync(ps)){
-	 this.imagesChecked = false;
-	 return Promise.reject();
+      let ps = p.replace("https://oversightmachin.es", "/mnt/oversee");
+      if (!fs.existsSync(ps)) {
+         this.imagesChecked = false;
+         return Promise.reject();
       }
    }
    this.imagesChecked = true;
    return Promise.resolve();
 }
 
-Doc.prototype.checkOCR = async function(){
+Doc.prototype.checkOCR = async function () {
 
    let findingsUrl = "https://oversightmachin.es/findings/slash/";
    let findingsPath = "/mnt/oversee/findings/slash/";
-   for (let m of this.modes){
+   for (let m of this.modes) {
       console.log("checking for", this.shortName, m);
       let fn = `${findingsPath}${this.shortName}_${m}.pdf`
-      if (fs.existsSync(fn)){
-	this.completedModes.push(m);
+      if (fs.existsSync(fn)) {
+         this.completedModes.push(m);
       }
    }
    return Promise.resolve();
 }
 
-let doTheThing = async function() {
-  console.log("getting data");
-  await getData();
-  console.log("making docs");
-  await makeHearDocs();
-  console.log("checking docs");
-  await checkHearDocs();
-  console.log(hDocs.length, "docs");
-  fs.writeFileSync(`${docspath}hdocs.json`, JSON.stringify(hDocs, undefined, 2));
+let doTheThing = async function () {
+   console.log("getting data");
+   await getData();
+   console.log("making docs");
+   await makeHearDocs();
+   console.log("checking docs");
+   await checkHearDocs();
+   console.log(hDocs.length, "docs");
+   fs.writeFileSync(`${docspath}hdocs.json`, JSON.stringify(hDocs, undefined, 2));
 };
 
 doTheThing();
