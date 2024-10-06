1ea9ad96f57685d6bb7caa623587c4d50dc90593
abram stern (aphid) <aphid@ucsc.edu>
2024-10-05 21:13:24 -0700

    add font scaling

--- pdfmerge.js
+++ pdfmerge.js
@@ -1,11 +1,20 @@
 let fs = require('fs');
 let puppeteer = require('puppeteer');
 const PDFMerge = require('pdf-merge');
+let exif = require("exiftool-vendored").exiftool;
 
 
+let title = "/mnt/oversee/findings/slash/171101_0930_Exhibits_used_by_Vice_Chairman_Warner_during_the_2017-11-01_hearing_tesseract_2.1.1.pdf";
+let metaFontSize = 18;
 let dothething = async function(){
+    await mergePDFs(title, metaFontSize);
+    process.exit();
+}
+
+let mergePDFs = async function(pdft, fontsize = 9){
+    //page length at sizes.  9: 5243  10.5?: 2966
     console.log("lets goooo");
-    let theJSON = fs.readFileSync("/mnt/oversee/findings/slash/170112_1000_questionnaire-011217_tesseract_2.1.1.pdf.json", "utf8").replace(/\\/g, "");
+    let theJSON = fs.readFileSync(`${pdft}.json`, "utf8").replace(/\\/g, "");
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
     page.on('console', async (msg) => {
@@ -14,26 +23,41 @@ let dothething = async function(){
             console.log(await msgArgs[i].jsonValue());
         }
     });
+    console.log(fontsize);
+    var fsz = fontsize;
     let url = "https://oversightmachin.es/ocr/metadata_template.html";
     await page.goto(url, { waitUntil: 'networkidle0' });
-    let ev = await page.evaluate((theJSON) => {
+    let ev = await page.evaluate((theJSON, fsz) => {
         //console.log(document.body.innerHTML);
-        let dom = document.querySelector('div');
+        let dom = document.querySelector('#metadata');
         //console.log(dom);
+	dom.style.fontSize = fsz + "pt";
         dom.innerHTML = theJSON;
-    }, theJSON);
+	console.log(dom.clientHeight);
+    }, theJSON, fsz);
+    let tmpfile = "temp_merged.pdf";
     let pdf = await page.pdf({
-	    path: "temp_merged.pdf",
+	    path: tmpfile,
 	    format: "Letter",
 	    margin: {top: "0.75in", right: "0.75in", bottom: "0.75in", left: "0.75in"},
 	    printBackground: true
     });
-    let pdfs = ["/mnt/oversee/findings/slash/170112_1000_questionnaire-011217_tesseract_2.1.1.pdf", "./temp_merged.pdf"];
-    PDFMerge(pdfs, {output: "/mnt/oversee/findings/slash/print/170112_1000_questionnaire-011217_tesseract_2.1.1.pdf"});
+    let tmpmeta = await exif.read(tmpfile);
+    console.log(tmpmeta.PageCount);
+    if (tmpmeta.PageCount > 25 && metaFontSize > 8){
+       metaFontSize = metaFontSize - 0.5;
+       console.log("new size " + metaFontSize);
+       return mergePDFs(pdft, metaFontSize);
+    }
+
+    let pdfs = [pdft, "./temp_merged.pdf"];
+    let printpdf = pdft.replace("/slash/", "/slash/print/");
+    await PDFMerge(pdfs, {output: printpdf});
+    console.log("...and SCENE");
+    return Promise.resolve();
 }
 
 dothething();
-
 
 
 
